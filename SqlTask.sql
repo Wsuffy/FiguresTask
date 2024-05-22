f51f6453710dc61c62647ce870b54b8d46f88922
Alexander "Skade" Soldatenkov <alsoldatenkov692@gmail.com>
2024-05-22 19:05:45 +0300

    Sql Task Added

---
+++ SqlTask.sql
@@ -0,0 +1,11 @@
+﻿SELECT
+    p.ProductName,
+    c.CategoryName
+FROM
+    Products p
+        LEFT JOIN
+    ProductCategoryMap pcm ON p.ProductID = pcm.ProductID 
+        LEFT JOIN
+    ProductCategories c ON pcm.CategoryID = c.CategoryID
+ORDER BY
+    p.ProductName, c.CategoryName;
